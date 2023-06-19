8c60a602735961bac819b0367a3cb0523e294b28
TetianaAvd <shyshyavd@gmail.com>
2023-06-19 23:28:03 +0200

    init commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        int i = 5;
+         sum = a + b + c + d + i;
+        System.out.println(sum);
+    }
+}
